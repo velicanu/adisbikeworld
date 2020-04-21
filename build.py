ddc5ab9860a7b2bece724c27af37b4aa235abd12
Dragos Velicanu <dvelicanu@manifold.ai>
2020-04-21 11:41:04 -0400

    seperate html src and template files, prettier templates

--- build.py
+++ build.py
@@ -6,20 +6,21 @@ log = getLogger(__name__)
 
 templates = ["HEAD", "NAVBAR", "SCRIPT"]
 script_dir = os.path.dirname(os.path.realpath(__file__))
+html_folder = "src"
 
 html_files = [
     f
-    for f in next(os.walk(os.path.join(script_dir, "templates")))[2]
+    for f in next(os.walk(os.path.join(script_dir, html_folder)))[2]
     if f.endswith(".html")
 ]
 
 for html_filename in html_files:
-    with open(os.path.join(script_dir, "templates", html_filename)) as html_file:
+    with open(os.path.join(script_dir, html_folder, html_filename)) as html_file:
         html_string = html_file.read()
 
     for template in templates:
         with open(
-            os.path.join(script_dir, "templates", f"{template}.template")
+            os.path.join(script_dir, "templates", f"{template}.html")
         ) as template_file:
             template_payload = template_file.read()
             html_string = html_string.replace(

9a4c01d8ee184c66bc88dc5159e14e010eb32839
Dragos Velicanu <dvelicanu@manifold.ai>
2021-04-12 11:50:06 -0400

    speed up build time 3x mainly by parallelizing prettier

--- build.py
+++ build.py
@@ -13,19 +13,22 @@ html_files = [
     for f in next(os.walk(os.path.join(script_dir, html_folder)))[2]
     if f.endswith(".html")
 ]
+payloads = {}
+for template in templates:
+    with open(
+        os.path.join(script_dir, "templates", f"{template}.html")
+    ) as template_file:
+        payloads[template] = template_file.read()
 
 for html_filename in html_files:
     with open(os.path.join(script_dir, html_folder, html_filename)) as html_file:
         html_string = html_file.read()
 
     for template in templates:
-        with open(
-            os.path.join(script_dir, "templates", f"{template}.html")
-        ) as template_file:
-            template_payload = template_file.read()
-            html_string = html_string.replace(
-                f"<!-- {template}_TEMPLATE -->", template_payload
-            )
+        template_payload = payloads[template]
+        html_string = html_string.replace(
+            f"<!-- {template}_TEMPLATE -->", template_payload
+        )
 
     with open(os.path.join(script_dir, html_filename), "w") as output_html_file:
         log.info(f"Generating {html_filename}")
--- prettier.sh
+++ prettier.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+pret() {
+    prettier $1 > .$1.tmp.html
+    mv .$1.tmp.html $1
+}
+
 for i in `ls *.html`; do
-    prettier $i > .tmp.html
-    mv .tmp.html $i
+    pret $i &
 done
+wait
